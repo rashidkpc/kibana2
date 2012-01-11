d57b89ad6939536f0289dd0d63563fd48363b165
Rashid Khan <rashidkpc@gmail.com>
2012-01-11 13:33:34 -0700

    changed welcome message

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ if ($use_auth) include 'auth.php';
         <div class="container-fluid">
             <form class="pull-left" id='searchform' action="">
               <table class=formatting><tr> 
-              <td width='1%'><img src='images/kibana_small.png'></td>
+              <td width='1%'><center><img src='images/logo.png'></center></td>
               <td width='1%'>
               <select name="time" id=timeinput class=small>
                 <option value="15 minutes">Last 15m</option>
@@ -67,17 +67,19 @@ if ($use_auth) include 'auth.php';
     <div class="container-fluid">
       <div class="sidebar" id=sidebar>
         <div class="well">
-        <div id=fields>
-        <h2>Welcome</h2>
-        <p>Welcome to Kibana. Give me a few moments, I'm fetching some interesting data to display. Might be a minute. STOP YELLING AT ME!</p>
-        </div>
+        <div id=fields></div>
         <div id=analyze></div>
         </div>
       </div>
       <div class="content">
         <div>
             <p id=graphheader></p>
-            <div id=graph style='height: 100px;'></div>
+            <div id=graph style='height: 100px;'>
+                <div class=hero-unit><h1><img src=images/kibana_banner.png></h1><br><br>
+                <p><strong>Welcome to Kibana.</strong> Give me a few moments, I'm fetching some interesting data to display. It might be a minute, there could be lots of data in here. Consider the thousands, millions or billions of events I might be looking through, to find just the right ones for you. It's nothing, really. You're welcome.
+                </p>
+                </div>
+            </div>
             <div id=legend></div>
         </div>
         <div id=logs class=zebra-stripped></div>
