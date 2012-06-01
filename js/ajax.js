c1b9a8c3c26768f7f1e3e85b5aef304b041dfeac
kcrayon <github.com@crayon.at>
2012-05-31 17:28:01 -0700

    Handle null values in viewLog

--- js/ajax.js
+++ js/ajax.js
@@ -645,7 +645,7 @@ function viewLog(objid) {
     if ($.inArray(field, window.hashjson.fields) > -1) {
       selected = "logfield_selected";
     }
-    value = obj[field].toString();
+    value = (obj[field] != null) ?  obj[field].toString() : '';
     if (!(field.match(/^@cabin_/))) {
       trclass = (i % 2 == 0) ? 'class=alt' : '';
       str += "<tr " + trclass + ">" +
