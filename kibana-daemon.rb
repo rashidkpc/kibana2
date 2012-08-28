8755c2246f7fbe8055b0ea55457256da8eebaf9f
Rashid Khan <rashidkpc@gmail.com>
2012-08-28 09:45:03 -0700

    Added error text to kibana-daemon, made runable from anywhere

--- kibana-daemon.rb
+++ kibana-daemon.rb
@@ -3,9 +3,15 @@ require 'daemons'
 
 # Thanks to Patrick McKenzie http://www.kalzumeus.com/2010/01/15/deploying-sinatra-on-ubuntu-in-which-i-employ-a-secretary/
 # *** Make sure /var/spool/sinatra/pid exists ***
+pid_dir = "/var/spool/sinatra/pid"
 
-pwd = Dir.pwd
-Daemons.run_proc('kibana.rb', {:dir_mode => :normal, :dir => "/var/spool/sinatra/pid"}) do
-  Dir.chdir(pwd)
-  exec "ruby kibana.rb"
+if File.directory? pid_dir
+  pwd = File.dirname(__FILE__) 
+  Daemons.run_proc('kibana.rb', {:dir_mode => :normal, :dir => pid_dir}) do
+    Dir.chdir(pwd)
+    exec "ruby kibana.rb"
+  end
+else
+  puts "#{pid_dir} does not exist."
+  puts "Please create it or change pid_dir in #{__FILE__}"  
 end
