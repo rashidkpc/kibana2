8f6e38489eb5152d23a41df4ea238b61c617a414
Matt Mencel <matt@techminer.net>
2012-08-28 10:20:20 -0500

    Linked to source site

--- kibana-daemon.rb
+++ kibana-daemon.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'daemons'
 
-#  Make sure /var/spool/sinatra/pid exists
+# Thanks to Patrick McKenzie http://www.kalzumeus.com/2010/01/15/deploying-sinatra-on-ubuntu-in-which-i-employ-a-secretary/
+# *** Make sure /var/spool/sinatra/pid exists ***
 
 pwd = Dir.pwd
 Daemons.run_proc('kibana.rb', {:dir_mode => :normal, :dir => "/var/spool/sinatra/pid"}) do
