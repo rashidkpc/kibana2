765225b905c70837c27e3a5cb9bf177b164a1395
Rashid Khan <rashidkpc@gmail.com>
2012-08-30 13:38:51 -0700

    Changed default elasticsearch host to localhost to avoid confusion

--- KibanaConfig.rb
+++ KibanaConfig.rb
@@ -2,7 +2,7 @@ module KibanaConfig
   # Your elastic search server(s). This may be set as an array for round robin
   # load balancing
   # Elasticsearch = ["elasticsearch1:9200","elasticsearch2:9200"]
-  Elasticsearch = "elasticsearch:9200"
+  Elasticsearch = "localhost:9200"
 
   # The port Kibana should listen on
   KibanaPort = 5601
