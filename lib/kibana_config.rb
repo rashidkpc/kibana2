6b88287b5d288d5198d6bddb50dfb987fdef83a9
Fabio Napoleoni <f.napoleoni@gmail.com>
2012-10-16 18:11:00 +0200

    Added load! call to avoid potential issues with sinatra

--- lib/kibana_config.rb
+++ lib/kibana_config.rb
@@ -1,4 +1,5 @@
 class KibanaConfig < Settingslogic
   source File.join(File.dirname(__FILE__), '../config', 'config.yml')
   namespace ENV['SINATRA_ENV'] ? ENV['SINATRA_ENV'] : 'defaults'
+  load!
 end
