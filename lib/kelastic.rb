rashidkpc/kibana2

commit 82992a260badf922cd0177191a28d9d38f85a34c

fixing index_range requested bug. from should not increment, only step time should

lib/kelastic.rb
@@ -57,6 +57,7 @@ class Kelastic
 
     # Returns list of index-date names which intersect with range defined by from and to
     def index_range(from,to,limit = -1)
+      step_time = from
       if KibanaConfig::Smart_index == true
       	index_pattern = "logstash-%Y.%m.%d"
       	if KibanaConfig::Smart_index_pattern != ""
@@ -67,7 +68,7 @@ class Kelastic
         for index in index_pattern do
           begin
             requested << from.getutc.strftime(index)
-          end while (from += KibanaConfig::Smart_index_step) <= to
+          end while (step_time += KibanaConfig::Smart_index_step) <= to
           requested << to.getutc.strftime(index) unless requested.include? to.getutc.strftime(index)
         end
 

commit e9c320cb403c80b247057f7de6149dfe11ba853a

fix index_range bug introduced in commit 82992a260badf922cd0177191a28d9d38f85a34c

lib/kelastic.rb
@@ -67,7 +67,7 @@ class Kelastic
         index_pattern = index_pattern.kind_of?(Array) ? index_pattern : [index_pattern]
         for index in index_pattern do
           begin
-            requested << from.getutc.strftime(index)
+            requested << step_time.getutc.strftime(index)
           end while (step_time += KibanaConfig::Smart_index_step) <= to
           requested << to.getutc.strftime(index) unless requested.include? to.getutc.strftime(index)
         end